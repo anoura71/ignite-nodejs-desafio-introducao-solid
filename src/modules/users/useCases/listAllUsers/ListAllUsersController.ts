22cbe5ae1167b569662ffae1c6184983aba6e1fa
Artur Noura Teixeira <artur.teixeira@serpro.gov.br>
2021-03-23 23:43:41 -0300

    OK

--- src/modules/users/useCases/listAllUsers/ListAllUsersController.ts
+++ src/modules/users/useCases/listAllUsers/ListAllUsersController.ts
@@ -1,12 +1,24 @@
 import { Request, Response } from "express";
+import { User } from "modules/users/model/User";
 
 import { ListAllUsersUseCase } from "./ListAllUsersUseCase";
 
 class ListAllUsersController {
-  constructor(private listAllUsersUseCase: ListAllUsersUseCase) {}
+  constructor(private listAllUsersUseCase: ListAllUsersUseCase) { }
 
   handle(request: Request, response: Response): Response {
-    // Complete aqui
+    const { user_id } = request.headers;
+
+    let users: User[] = [];
+    try {
+      const id = String(user_id);
+
+      users = this.listAllUsersUseCase.execute({ user_id: id });
+    } catch (error) {
+      return response.status(400).json({ error: error.message });
+    }
+
+    return response.json(users);
   }
 }
 
